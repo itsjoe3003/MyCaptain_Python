85dbbc8810ef93c3b356c25bc416be0b56ab2cc5
itsjoe3003 <83822751+itsjoe3003@users.noreply.github.com>
2021-05-08 12:47:55 +0530

    Delete CircleArea.py

--- CircleArea.py
+++
@@ -1,5 +0,0 @@
-rad = float(input("Input the radius of the circle: "))   #input for radius which is converted to float type
-
-area = (22/7) * rad * rad                                #area is calculated using formula for area of a circle 
-
-print("Area of the circle with radius {} is: {}".format(rad,area))          #the area of the circle is displayed along with the radius 
